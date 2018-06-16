3515cf4c8b71f447769ee0ba4b2c2d9ee076bc95
Mikhail Babich <babich-mv@yandex.ru>
2018-06-17 03:44:09 +1000

    Add belt

---
+++ Assets/Resources/Scripts/TransformFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class TransformFollow : VRTK_TransformFollow {
+    protected override void SetRotationOnGameObject(Quaternion newRotation)
+    {
+        newRotation.x = transformToChange.rotation.x;
+        newRotation.z = transformToChange.rotation.z;
+        transformToChange.rotation = newRotation;
+    }
+}
